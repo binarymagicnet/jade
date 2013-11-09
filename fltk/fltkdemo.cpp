d2b33b41d645ad2618efdaeb2041f3d762e5ba41
Armando Rivera <Airrsongs@gmail.com>
2013-11-09 14:26:22 -0500

    New FLTK Demo

---
+++ fltk/fltkdemo.cpp
@@ -0,0 +1,24 @@
+#include "../jade.h"
+
+#include "fltk.inc"
+
+
+MAIN
+  DIM AS INT top = 40;
+  DIM AS WINDOW *win;
+  DIM AS INPUT *txtSource, *txtDest;
+  DIM AS BUTTON *btnSource, *btnDest;
+
+  win = new WINDOW(690,486,"FLTK Demo");
+  win->position((Fl::w() - win->w())/2, (Fl::h() - win->h())/2);
+  txtSource = new INPUT(20,top,540,26);
+  btnSource = new BUTTON(580,top,90,26,"Source");
+  txtDest = new INPUT(20,top+40,540,26);
+
+  btnDest = new BUTTON(580, top+40,90,26,"Dest");
+  win->show();
+  RETURN Fl::run();
+ENDMAIN
+
+
+
