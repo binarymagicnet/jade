5faed27d058fd360fd7ef5b2475f234f429b5ea3
Armando Rivera <Airrsongs@gmail.com>
2013-11-10 20:34:51 -0500

    Updated Keywords.txt and generate-keywords script

--- generate-keywords.sh
+++ generate-keywords.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # KEYWORDS
-KEYWORDS=$(ack '#define (\w+.|\w+)' cppbas.inc  --output='$1')
-KEYWORDS+=$(ack '^FUNCTION \w+ (\w+.|\w+)' cppbasrt.inc  --output='$1')
-KEYWORDS+='\n'$(ack '^SUB (\w+)' cppbasrt.inc  --output='$1')
+KEYWORDS=$(ack '#define (\w+.|\w+)' header.inc  --output='$1')
+KEYWORDS+=$(ack '^FUNCTION \w+ (\w+.|\w+)' runtime.inc  --output='$1')
+KEYWORDS+='\n'$(ack '^SUB (\w+)' runtime.inc  --output='$1')
 
 # FUNCTION/SUB PROTOTYPES
-PROTO=$(ack '^FUNCTION \w+ (.+\))' cppbasrt.inc  --output='$1')' '
-PROTO+='\n'$(ack '^SUB (.+\))' cppbasrt.inc  --output='$1')
+PROTO=$(ack '^FUNCTION \w+ (.+\))' runtime.inc  --output='$1')' '
+PROTO+='\n'$(ack '^SUB (.+\))' runtime.inc  --output='$1')
 #
 
 # SAVE SORTED KEYWORDS/PROTOTYPES
