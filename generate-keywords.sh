bedc0b9dccdf53121a99e78d667b7bb2dba8d058
Armando Rivera <Airrsongs@gmail.com>
2013-11-15 23:05:09 -0500

    Fixed generate-keywords script

--- generate-keywords.sh
+++ generate-keywords.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # KEYWORDS
-KEYWORDS=$(ack '#define (\w+.|\w+)' header.inc  --output='$1')
-KEYWORDS+=$(ack '^FUNCTION \w+ (\w+.|\w+)' runtime.inc  --output='$1')
-KEYWORDS+='\n'$(ack '^SUB (\w+)' runtime.inc  --output='$1')
+KEYWORDS=$(ack '#define (\w+.|\w+)' header.inc  --output='$1')'\n'
+KEYWORDS+=$(ack '^FUNCTION \w+ (\w+.|\w+)' runtime.inc  --output='$1')'\n'
+KEYWORDS+=$(ack '^SUB (\w+)' runtime.inc  --output='$1')
 
 # FUNCTION/SUB PROTOTYPES
-PROTO=$(ack '^FUNCTION \w+ (.+\))' runtime.inc  --output='$1')' '
-PROTO+='\n'$(ack '^SUB (.+\))' runtime.inc  --output='$1')
+PROTO=$(ack '^FUNCTION \w+ (.+\))' runtime.inc  --output='$1')'\n'
+PROTO+=$(ack '^SUB (.+\))' runtime.inc  --output='$1')
 #
 
 # SAVE SORTED KEYWORDS/PROTOTYPES
